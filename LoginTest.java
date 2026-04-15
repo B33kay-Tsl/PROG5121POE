8243b0520e39f96c9a447824be242bfb9ac5bccc
B33kay-Tsl <ST10511967@rcconnect.edu.za>
2026-04-15 16:31:39 +0200

    login test upload

---
+++ LoginTest.java
@@ -0,0 +1,69 @@
+package prog5121poe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void testUsernameCorrectlyFormatted() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertTrue(login.checkUserName());
+    }
+
+    @Test
+    public void testUsernameIncorrectlyFormatted() {
+        Login login = new Login("Kyle", "Smith", "kyle!!!!!!!", "Ch&&sec@ke99!", "+27821234567");
+        assertFalse(login.checkUserName());
+    }
+
+    @Test
+    public void testPasswordMeetsComplexity() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertTrue(login.checkPasswordComplexity());
+    }
+
+    @Test
+    public void testPasswordDoesNotMeetComplexity() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "password", "+27821234567");
+        assertFalse(login.checkPasswordComplexity());
+    }
+
+    @Test
+    public void testCellPhoneCorrectlyFormatted() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertTrue(login.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testCellPhoneIncorrectlyFormatted() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "08966553");
+        assertFalse(login.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testLoginSuccessful() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
+    }
+
+    @Test
+    public void testLoginFailed() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertFalse(login.loginUser("kyl_1", "wrongpassword"));
+    }
+
+    @Test
+    public void testRegisterUserSuccess() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&&sec@ke99!", "+27821234567");
+        assertEquals("Welcome Kyle Smith it is great to see you.", login.registerUser());
+    }
+
+    @Test
+    public void testRegisterUserBadUsername() {
+        Login login = new Login("Kyle", "Smith", "kyle!!!!!!!", "Ch&&sec@ke99!", "+27821234567");
+        assertEquals("Username is not correctly formatted; please ensure that your " +
+                "username contains an underscore and is no more than five " +
+                "characters in length.", login.registerUser());
+    }
+}
